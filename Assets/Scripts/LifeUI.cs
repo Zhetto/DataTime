85b321bd525d9deda1306653341dea7b34cb541b
Gabriel Bueno da Silva Rosa <gabrielbs98@hotmail.com>
2021-06-06 23:57:54 -0300

    Insert Life

---
+++ Assets/Scripts/LifeUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeUI : MonoBehaviour
+{
+    [SerializeField] Text txt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt.text = Checkpoint.restLife.ToString();    
+    }
+}
